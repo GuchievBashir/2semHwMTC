5a41e5d42edcf78c7edaedd73ce1cb241e10f082
Guchiev Magomed-Bashir <magomedbashir095@gmail.com>
2025-02-24 01:08:59 +0300

    Add tests, fix exceptions

--- src/main/java/mipt/guchievmb/hw1/exception/BookNotFoundException.java
+++ src/main/java/mipt/guchievmb/hw1/exception/BookNotFoundException.java
@@ -1,5 +1,9 @@
 package mipt.guchievmb.hw1.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends RuntimeException {
   public BookNotFoundException(String id) {
     super("Книга с id " + id + " не найдена.");
